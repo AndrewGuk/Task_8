620c25a5968bfa9b860bec2774888b3a1e369a5e
Andrei Huk <belakolala1@gmail.com>
2022-03-15 01:46:14 +0300

    Add project files.

---
+++ Task_8_Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using Printers;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var i = new GreenPrinter("asd");
+            var f = new Printer("sada");
+            var s = new BluePrinter("sdf");
+            Room room = new Room(i,"fsd");
+            //room.printer = i;
+            room.MakeThisLazyPrinterWork("dfsf");
+            //room.printer = f;
+            room.MakeThisLazyPrinterWork("6544545");
+            //room.printer = s;
+            room.MakeThisLazyPrinterWork("dsgfgdf7878");
+            room.value = "asdad";
+            room.ChangePrinter(s, room.value);
+            room.ChangePrinter(f, room.value);
+            room.ChangePrinter(i, room.value);
+        }
+    }
+}
